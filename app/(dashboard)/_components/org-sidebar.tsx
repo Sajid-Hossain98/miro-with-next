11f9d34dd3ad6d4ef449f3aec6ebbdf77a005fa9
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-20 23:51:55 +0600

    logo and org switcher in the org sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,55 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
-      Org sidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-yellow-200">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image
+            src="/logo-mirowithnext.svg"
+            alt="logo"
+            height={60}
+            width={60}
+          />
+
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundClip: "white",
+            },
+          },
+        }}
+      />
     </div>
   );
 };
