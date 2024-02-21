544e8556ef8cedd09a680eeb08ddc09764f6e6c5
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-21 14:56:49 +0600

    team boards and favorite boards in the org sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard, Star } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -50,6 +52,32 @@ export const OrgSidebar = () => {
           },
         }}
       />
+
+      <div className="space-y-1 w-full">
+        <Button
+          variant="ghost"
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team boards
+          </Link>
+        </Button>
+
+        <Button
+          variant="ghost"
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href={{ pathname: "/", query: { favorites: true } }}>
+            <Star className="h-4 w-4 mr-2" />
+            Favorite boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
