b2467148210a0febab065e7283bb81cd4564fd5c
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-29 21:35:44 +0600

    using the useApiMutation hook and sonner setup

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -2,21 +2,27 @@
 
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useOrganization } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
   const { organization } = useOrganization();
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = () => {
     if (!organization) return;
 
-    create({
+    mutate({
       orgId: organization.id,
       title: "Untitled",
-    });
+    })
+      .then((id) => {
+        toast.success("Board created!");
+        //TODO: Redirect to board/{id}
+      })
+      .catch(() => toast.error("Failed to create board!"));
   };
 
   return (
@@ -29,7 +35,7 @@ export const EmptyBoards = () => {
       </p>
 
       <div className="mt-6">
-        <Button onClick={onClick} size="lg">
+        <Button disabled={pending} onClick={onClick} size="lg">
           Create board
         </Button>
       </div>
