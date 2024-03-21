6283cc3702bad4dbcc57eeb440443c8245a32d76
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-03-21 19:06:04 +0600

    ui to input to rename a modal

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -3,14 +3,27 @@
 import { useRenameModal } from "@/store/use-rename-modal";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "../ui/dialog";
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export const RenameModal = () => {
   const { isOpen, onClose, initialValues } = useRenameModal();
+
+  const [title, setTitle] = useState(initialValues.title);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const onSubmit = () => {};
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -21,6 +34,29 @@ export const RenameModal = () => {
           <DialogDescription>
             Enter a new title for this board
           </DialogDescription>
+
+          <form onSubmit={onSubmit} className="space-y-4">
+            <Input
+              disabled={false}
+              required
+              maxLength={60}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Board title"
+            />
+
+            <DialogFooter className="gap-4">
+              <DialogClose asChild>
+                <Button type="button" variant="outline">
+                  Cancel
+                </Button>
+              </DialogClose>
+
+              <Button disabled={false} type="submit">
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
         </DialogHeader>
       </DialogContent>
     </Dialog>
