41ed3fbf3e947ba6bf572f83a662289b07a86a40
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-04-04 20:55:06 +0600

    Initial canvas layout setup

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,7 @@
+export const Participants = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md flex items-center shadow-md p-3">
+      List of users
+    </div>
+  );
+};
