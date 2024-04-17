98584377f89ce1310ed3af238776d7f165c5831d
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-04-17 19:35:10 +0600

    loading skeleton for participants

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md flex items-center shadow-md p-3">
       List of users
+    </div>
+  );
+};
+
+Participants.Skeleton = function ParticipantSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md flex items-center shadow-md p-3 w-[100px]">
+      <Skeleton className="h-full w-full bg-muted-400" />
     </div>
   );
 };
