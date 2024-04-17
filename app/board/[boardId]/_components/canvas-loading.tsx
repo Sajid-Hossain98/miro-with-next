0b566c03c7a632de2d3842c4aea689f78e6b937c
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-04-17 20:24:48 +0600

    skeleton for toolbar

--- app/board/[boardId]/_components/canvas-loading.tsx
+++ app/board/[boardId]/_components/canvas-loading.tsx
@@ -1,6 +1,7 @@
 import { Loader } from "lucide-react";
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
 
 export const CanvasLoading = () => {
   return (
@@ -8,6 +9,7 @@ export const CanvasLoading = () => {
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
       <Info.Skeleton />
       <Participants.Skeleton />
+      <Toolbar.Skeleton />
     </main>
   );
 };
