6f62360812369138cd795fd059a6148233b1d9cc
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-04-19 23:19:52 +0600

    api route for liveblocks

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,42 @@
+import { api } from "@/convex/_generated/api";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret:
+    "sk_dev_HvCGoX28e2yYnZtP2DwH_HZ73aC7TGma1RTagOHZGNHoz4_qd9bdfBeayKpHB4hf",
+});
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized");
+  }
+
+  const userInfo = {
+    name: user.firstName || "Anonymous",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+}
