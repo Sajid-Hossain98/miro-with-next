4fa150111bd579c9f1ac7bad7a575775ca7a7216
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-04-20 20:51:20 +0600

    modifying info, participants & toolbar components

--- app/board/[boardId]/_components/canvas-loading.tsx
+++ app/board/[boardId]/_components/canvas-loading.tsx
@@ -1,15 +1,15 @@
 import { Loader } from "lucide-react";
-import { Info } from "./info";
-import { Participants } from "./participants";
-import { Toolbar } from "./toolbar";
+import { InfoSkeleton } from "./info";
+import { ParticipantSkeleton } from "./participants";
+import { ToolbarSkeleton } from "./toolbar";
 
 export const CanvasLoading = () => {
   return (
     <main className="h-full w-full relative border-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <InfoSkeleton />
+      <ParticipantSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -6,7 +6,7 @@ export const Participants = () => {
   );
 };
 
-Participants.Skeleton = function ParticipantSkeleton() {
+export const ParticipantSkeleton = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md flex items-center shadow-md p-3 w-[100px]" />
   );
